a58a82356ec37c2789385d8878cbaf4670e8beb7
Justin <justin.worrall@gmail.com>
2014-08-06 17:33:06 +0100

    refactored chat component

--- src/chat_controller.erl
+++ src/chat_controller.erl
@@ -2,21 +2,23 @@
 
 -export([handle/1]).
 
+-define(BLANK, "\r\n").
+
 %% http://stackoverflow.com/questions/853296/remove-whitespace-using-erlang-regex
 
 clean(Input) ->
     case re:replace(Input, "\\r?\\n$", "") of
 	[Output, []] ->
-	    binary_to_list(Output);
+	    Output;
 	_ ->
-	    Input
+	    list_to_binary(Input)
     end.
 
 handle(RawData) ->
     case RawData of
-	"\r\n" ->
+	?BLANK ->
 	    "No data received\n";	
 	_ ->
 	    CleanData = clean(RawData),
-	    io_lib:fwrite("You sent: ~p~n", [CleanData])
+	    io_lib:fwrite("You sent: ~p~n", [binary_to_list(CleanData)])
     end.
