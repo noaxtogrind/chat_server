2fbc0b97819a629f8548198a956896b0ff8f54d2
Justin <justin.worrall@gmail.com>
2014-08-06 17:26:14 +0100

    added message cleaning

--- src/chat_controller.erl
+++ src/chat_controller.erl
@@ -2,7 +2,21 @@
 
 -export([handle/1]).
 
-%% simple echo pattern
+%% http://stackoverflow.com/questions/853296/remove-whitespace-using-erlang-regex
+
+clean(Input) ->
+    case re:replace(Input, "\\r?\\n$", "") of
+	[Output, []] ->
+	    binary_to_list(Output);
+	_ ->
+	    Input
+    end.
 
 handle(RawData) ->
-    io_lib:fwrite("You sent: ~p~n", [RawData]).
+    case RawData of
+	"\r\n" ->
+	    "No data received\n";	
+	_ ->
+	    CleanData = clean(RawData),
+	    io_lib:fwrite("You sent: ~p~n", [CleanData])
+    end.
